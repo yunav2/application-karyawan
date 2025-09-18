982b201943a87cd6aa47c532009357defa5d60cc
yuuX <yunaodasteam@gmail.com>
2025-09-18 13:46:31 +0700

    aplikasi

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 # Ganti dengan detail koneksi database MySQL Anda
 DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',         # Ganti dengan username database Anda
-    'password': '',         # Ganti dengan password database Anda
-    'database': 'joudahgroup' # Nama database yang sudah Anda buat
+    'host': 'srv1785.hstgr.io',
+    'user': 'u451360885_joudah',         # Ganti dengan username database Anda
+    'password': 'Joudah123#',         # Ganti dengan password database Anda
+    'database': 'u451360885_joudah_group' # Nama database yang sudah Anda buat
 }
