47937ff6056d642a42cdcbcee450ecc0def14a49
yuuX <yunaodasteam@gmail.com>
2025-09-18 14:10:23 +0700

    ganti ip

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 # Ganti dengan detail koneksi database MySQL Anda
 DB_CONFIG = {
-    'host': 'srv1785.hstgr.io',
+    'host': '153.92.15.44',
     'user': 'u451360885_joudah',         # Ganti dengan username database Anda
     'password': 'Joudah123#',         # Ganti dengan password database Anda
     'database': 'u451360885_joudah_group' # Nama database yang sudah Anda buat
